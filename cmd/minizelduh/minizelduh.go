994de199f715fda667c1f39634b44985fc307436
Michael Moore <michael.moore@contrastsecurity.com>
2018-02-27 23:54:17 -0500

    Title screen

---
+++ cmd/minizelduh/minizelduh.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+	"github.com/faiface/pixel/text"
+	"golang.org/x/image/colornames"
+)
+
+var title string = "Mini Zelduh"
+
+func run() {
+	// Setup Text
+	orig := pixel.V(20, 50)
+	txt := text.New(orig, text.Atlas7x13)
+	txt.Color = colornames.White
+
+	// Setup GUI window
+	cfg := pixelgl.WindowConfig{
+		Title:  title,
+		Bounds: pixel.R(0, 0, 400, 225),
+		VSync:  true,
+	}
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	// win.SetSmooth(true)
+
+	fmt.Fprintln(txt, title)
+	for !win.Closed() {
+		txt.Draw(win, pixel.IM.Moved(win.Bounds().Center().Sub(txt.Bounds().Center())))
+		win.Update()
+	}
+}
+
+func main() {
+	pixelgl.Run(run)
+}
