b11170199e9fe8fbe42023d34966dad7ee0c1971
Michael Moore <michael.moore@contrastsecurity.com>
2018-04-16 21:26:37 -0400

    :construction: bomb config

--- cmd/zelduh/zelduh.go
+++ cmd/zelduh/zelduh.go
@@ -189,6 +189,26 @@ var entityPresets = map[string]enemyPresetFn{
 			Ignore: true,
 		}
 	},
+	"bomb": func(xTiles, yTiles float64) rooms.EntityConfig {
+		return rooms.EntityConfig{
+			Category: categories.Bomb,
+			Movement: &rooms.MovementConfig{
+				Direction: direction.Down,
+				Speed:     0.0,
+			},
+			W: s,
+			H: s,
+			X: s * xTiles,
+			Y: s * yTiles,
+			Animation: rooms.AnimationConfig{
+				"default": []int{138, 139, 140, 141},
+			},
+			Hitbox: &rooms.HitboxConfig{
+				Radius: 5,
+			},
+			Ignore: true,
+		}
+	},
 	"coin": func(xTiles, yTiles float64) rooms.EntityConfig {
 		return rooms.EntityConfig{
 			Category: categories.Coin,
@@ -564,7 +584,7 @@ func run() {
 
 	// Build entities
 	player := buildEntityFromConfig(entityPresets["player"](6, 6), gameWorld.NewEntityID())
-	bomb := NewBomb(gameWorld.NewEntityID(), 0.0, 0.0, s, s)
+	bomb := buildEntityFromConfig(entityPresets["bomb"](0, 0), gameWorld.NewEntityID())
 	explosion := buildEntityFromConfig(entityPresets["explosion"](0, 0), gameWorld.NewEntityID())
 	sword := buildEntityFromConfig(entityPresets["sword"](0, 0), gameWorld.NewEntityID())
 	arrow := buildEntityFromConfig(entityPresets["arrow"](0, 0), gameWorld.NewEntityID())
@@ -1130,36 +1150,6 @@ func loadPicture(path string) pixel.Picture {
 		os.Exit(1)
 	}
 	return pixel.PictureDataFromImage(img)
-}
-
-// NewBomb builds a bomb entity
-func NewBomb(id entities.EntityID, x, y, w, h float64) entities.Entity {
-	return entities.Entity{
-		ID:       id,
-		Category: categories.Bomb,
-		Ignore: &components.Ignore{
-			Value: true,
-		},
-		Appearance: &components.Appearance{
-			Color: colornames.Deeppink,
-		},
-		Spatial: &components.Spatial{
-			Width:        w,
-			Height:       h,
-			Rect:         pixel.R(x, y, x+w, y+h),
-			Shape:        imdraw.New(nil),
-			HitBox:       imdraw.New(nil),
-			HitBoxRadius: 5,
-		},
-		Animation: &components.Animation{
-			Map: components.AnimationMap{
-				"default": &components.AnimationData{
-					Frames:    []int{138, 139, 140, 141},
-					FrameRate: frameRate,
-				},
-			},
-		},
-	}
 }
 
 func addEntityToSystem(entity entities.Entity) {
