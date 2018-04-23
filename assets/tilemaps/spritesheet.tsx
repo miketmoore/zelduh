db7027f2e1a1a6472c8324700641cec9605f9496
Michael Moore <michael.moore@contrastsecurity.com>
2018-04-22 21:58:24 -0400

    :art: add assets

---
+++ assets/tilemaps/spritesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="spritesheet" tilewidth="48" tileheight="48" tilecount="210" columns="15">
+ <image source="../spritesheet.png" width="720" height="672"/>
+</tileset>
