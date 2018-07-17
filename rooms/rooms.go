e215eb0619f9e61f7e12f890e1a46ec6fa7b09fb
Michael Moore <michael.moore@contrastsecurity.com>
2018-07-16 21:57:46 -0400

    move rooms code to own file

---
+++ rooms/rooms.go
@@ -0,0 +1,46 @@
+package rooms
+
+import "github.com/miketmoore/terraform2d"
+
+// Rooms is a type of map that indexes rooms by their ID
+type Rooms map[terraform2d.RoomID]terraform2d.Room
+
+// Room represents one map section
+type Room struct {
+	mapName        string
+	connectedRooms *terraform2d.ConnectedRooms
+	EntityConfigs  []EntityConfig
+}
+
+// MapName returns the room's map name
+func (r Room) MapName() string {
+	return r.mapName
+}
+
+// ConnectedRooms returns the room's map name
+func (r Room) ConnectedRooms() *terraform2d.ConnectedRooms {
+	return r.connectedRooms
+}
+
+// SetConnectedRoom sets the connected room IDs
+func (r Room) SetConnectedRoom(direction terraform2d.Direction, id terraform2d.RoomID) {
+	switch direction {
+	case terraform2d.DirectionUp:
+		r.connectedRooms.Top = id
+	case terraform2d.DirectionRight:
+		r.connectedRooms.Right = id
+	case terraform2d.DirectionDown:
+		r.connectedRooms.Bottom = id
+	case terraform2d.DirectionLeft:
+		r.connectedRooms.Left = id
+	}
+}
+
+// NewRoom builds a new Room
+func NewRoom(name string, entityConfigs ...EntityConfig) *Room {
+	return &Room{
+		mapName:        name,
+		connectedRooms: &terraform2d.ConnectedRooms{},
+		EntityConfigs:  entityConfigs,
+	}
+}
